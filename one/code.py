35634f92aae00ed4ac68fcf7bc49a49843c6a949
SalmaAlgayar <salmaalgayar47@gmail.com>
2025-07-13 14:36:28 +0300

    Create code.py

---
+++ one/code.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+#1
+df = pd.read_csv(r"d.csv")
+df["Join_Date"]=pd.to_datetime(df["Join_Date"])
+
+#2
+df2 = df[(df["Salary"] > 5000) & (df["Join_Date"].dt.year >= 2020 )]
+
+
+#3
+df3 = df2.groupby("Department")["Salary"].mean()
+
+#4
+df4 = df3.to_frame().sort_values(by = "Salary", ascending = False)
+print(df4)
